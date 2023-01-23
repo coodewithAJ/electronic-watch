4709c99fe3910a81cc5070092a7c356e8c36a62e
ashok <ashok03012000@gmail.com>
2023-01-23 15:40:37 +0530

    changes

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,6 +6,11 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int hours = seconds/3600;
+        int minutes = (seconds%3600)/60;
+        int remSeconds = seconds % 60;
+
+        System.out.println(String.format("%02d:%02d:%02d", hours, minutes, remSeconds));
 
 
     }
